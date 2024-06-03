5f6950dc2d41d7b4e3557a7cb61c0ea3f916dbd6
UtopicRay <ray2021@gmail.com>
2024-06-03 00:38:55 -0400

    'Completada la Funcion de updateCantidadProduct'

--- src/store/ShopCartStore.js
+++ src/store/ShopCartStore.js
@@ -6,20 +6,37 @@ import "vue3-toastify/dist/index.css";
 export const useShopCartStore = defineStore("shopCartStore", () => {
   const count = ref(0);
   const price = ref(0);
-  const products = ref([{ item: {} ,cantidad: 0 }]);
+  const products = ref([]);
   function addProduct(product) {
     products.value.push({
-      cantidad:0
+      item: { ...product },
+      cantidad: 1, // Asumiendo que la cantidad inicial es 1
     });
-    console.log(products.value[0]);
     toast.success("Producto Añadido al Carrito");
     count.value = count.value + 1;
   }
+  function updateCantidadProduct(product, order) {
+    if (order === "less") {
+      if (product.cantidad === 1) {
+        return;
+      }
+      product.cantidad = product.cantidad - 1;
+    } else {
+      product.cantidad = product.cantidad + 1;
+    }
+  }
   function RemoveProduct(id) {
     console.log(products);
-    products.value = products.value.filter((product) => product.id != id);
+    products.value = products.value.filter((product) => product.item.id != id);
     toast.error("Producto eliminado del Carrito");
     count.value = count.value - 1;
   }
-  return { count, products, addProduct, RemoveProduct, price };
+  return {
+    count,
+    products,
+    addProduct,
+    RemoveProduct,
+    price,
+    updateCantidadProduct,
+  };
 });
