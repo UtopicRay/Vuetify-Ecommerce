207a9a58bcf1fd32ebc37ce752a53139a148790b
UtopicRay <ray2021@gmail.com>
2024-06-03 12:41:01 -0400

    'Add a MainLayout'

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import AppFooter from "@/components/AppFooter.vue";
+</script>
+<template>
+    <v-app>
+        <Navbar></Navbar>
+        <v-main>
+          <slot></slot>
+        </v-main>
+        <AppFooter></AppFooter>
+    </v-app>
+</template>
