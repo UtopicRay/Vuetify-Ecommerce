d7b1b185f7cb80edd3a85e3d84dfa81c369f0e77
UtopicRay <ray2021@gmail.com>
2024-06-03 09:29:42 -0400

    'ShopCartItem Completado'

--- src/components/ShopCartItem.vue
+++ src/components/ShopCartItem.vue
@@ -4,17 +4,24 @@ const shopCart = useShopCartStore();
 const props = defineProps({ item: Array});
 </script>
 <template>
-  <v-card width="600" class="bg-grey-darken-3 mx-2 my-2">
+  <v-card class="bg-grey-darken-3 mx-2 my-2">
     <v-row>
       <v-col cols="auto">
         <v-img :src="props.item.item.images[0]" width="200" height="200"></v-img>
       </v-col>
       <v-col cols="6">
         <v-card-title>{{ props.item.item.title }}</v-card-title>
-        <v-card-text>{{ props.item.item.description }}</v-card-text>
+        <v-card-text>
+          <p>{{ props.item.item.description }}</p>
+        <p class="mt-4 text-h4 font-weight-bold">{{ props.item.item.price*props.item.cantidad }}</p>
+        </v-card-text>
         <v-card-actions>
-          <v-btn color="primary" variant="flat">Comprar</v-btn>
-          <v-btn color="red" variant="elevated" @click="shopCart.RemoveProduct(props.item.id)"> Eliminar</v-btn>
+          <div class="d-flex justify-center align-center mx-2 ga-1">
+            <v-btn density="compact" icon="mdi-less" @click="shopCart.updateCantidadProduct(props.item,'less')" variant="elevated">-</v-btn>
+            <p>Cant: {{ props.item.cantidad }}</p>
+            <v-btn icon="mdi-plus" density="compact" @click="shopCart.updateCantidadProduct(props.item,'more')" variant="elevated"/>
+          </div>
+          <v-btn color="red" variant="elevated" @click="shopCart.RemoveProduct(props.item)"> Eliminar</v-btn>
         </v-card-actions>
       </v-col>
     </v-row>
