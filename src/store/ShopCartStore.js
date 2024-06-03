08ed40d18bb4e81fae434f3d0194f6812412100c
UtopicRay <ray2021@gmail.com>
2024-06-03 01:19:52 -0400

    'Añadiendo funcionalidades de los precios'

--- src/store/ShopCartStore.js
+++ src/store/ShopCartStore.js
@@ -8,12 +8,25 @@ export const useShopCartStore = defineStore("shopCartStore", () => {
   const price = ref(0);
   const products = ref([]);
   function addProduct(product) {
-    products.value.push({
-      item: { ...product },
-      cantidad: 1, // Asumiendo que la cantidad inicial es 1
-    });
-    toast.success("Producto Añadido al Carrito");
-    count.value = count.value + 1;
+    let aux = products.value.find(
+      (product1) => product1.item.id === product.id
+    );
+    if (aux) {
+      aux.cantidad = aux.cantidad + 1;
+      aux.price=aux.price+product.price;
+      count.value = count.value + 1;
+      price.value=price.value+product.price;
+      toast.success("Producto Añadido al Carrito");
+    } else {
+      products.value.push({
+        item: { ...product },
+        cantidad: 1,
+        price: product.price,
+      });
+      toast.success("Producto Añadido al Carrito");
+      count.value = count.value + 1;
+      price.value = price.value + product.price;
+    }
   }
   function updateCantidadProduct(product, order) {
     if (order === "less") {
@@ -21,12 +34,15 @@ export const useShopCartStore = defineStore("shopCartStore", () => {
         return;
       }
       product.cantidad = product.cantidad - 1;
+      price.value = price.value - product.price;
+      count.value = count.value - 1;
     } else {
       product.cantidad = product.cantidad + 1;
+      price.value = price.value + product.price;
+      count.value = count.value + 1;
     }
   }
   function RemoveProduct(id) {
-    console.log(products);
     products.value = products.value.filter((product) => product.item.id != id);
     toast.error("Producto eliminado del Carrito");
     count.value = count.value - 1;
